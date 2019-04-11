47951c0e66380046da454cb10a827230106f6d2c
Neli <diakonidzeneli@gmail.com>
2019-04-11 13:04:29 +0200

    Updates

---
+++ precache-manifest.fc3ae5e8c2bcfd08a248ce1dfe8f446d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75cd4691e507ebc50627",
+    "url": "/my-travels/static/js/runtime~main.20874d8d.js"
+  },
+  {
+    "revision": "08d49d32a79c74322254",
+    "url": "/my-travels/static/js/main.1094261a.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/my-travels/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "08d49d32a79c74322254",
+    "url": "/my-travels/static/css/main.8b9db527.chunk.css"
+  },
+  {
+    "revision": "70a05dd304cae8eb483b4e277966b5f8",
+    "url": "/my-travels/index.html"
+  }
+];
